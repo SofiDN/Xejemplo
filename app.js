c8519fdaff8c462361ebd3c1d250feaacb46f26e
SofiDN <93453759+SofiDN@users.noreply.github.com>
2021-12-22 11:20:04 -0300

    modifico links en app.js

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ app.listen(process.env.PORT || 3001,function(){
     console.log("servidor corriendo en puerto 3001")
 });
 
-app.use(express.static(path.resolve(__dirname,"public")))
+app.use(express.static(path.resolve(__dirname + "/public")));
 
 
-app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views/home.html")));
+app.get("/",(req,res)=>{ 
+res.sendFile(path.resolve(__dirname+"/views/home.html"))});
 
-app.get("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views/register.html")));
+app.get("/register",(req,res)=> {
+res.sendFile(path.resolve(__dirname+"/views/register.html"))},
 
-app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views/login.html")));
+app.get("/login",(req,res)=> {
+res.sendFile(path.resolve(__dirname +"/views/login.html"))}))
